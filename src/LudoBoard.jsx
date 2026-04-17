7b2c08b485938852c4c1f229dd70ce15bff8ac4e
Muhammad Aazmeer <aazmeer.official@gmail.com>
2026-04-17 16:20:17 +0500

    COMPLETED

---
+++ src/LudoBoard.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react"
+
+export default function LudoBoard(){
+    let [moves,setMoves] = useState({blue:0,red:0,yellow:0,green:0})
+    let updateBlue = ()=>{
+        setMoves((currValue)=>{
+            return({...currValue, blue: currValue.blue + 1})
+        })
+    }
+    return(
+        <div>
+            <p>Starting with game</p>
+            <br />
+            <div className="board">
+                <p>Blue moves = {moves.blue}</p>
+                <button style={{backgroundColor:"blue"}} onClick={updateBlue}>+1</button>
+                <p>Red moves = {moves.red}</p>
+                <button style={{backgroundColor:"red"}}>+1</button>
+                <p>Yellow moves = {moves.yellow}</p>
+                <button style={{backgroundColor:"yellow", color:"black"}}>+1</button>
+                <p>Green moves = {moves.green}</p>
+                <button style={{backgroundColor:"green"}}>+1</button>
+            </div>
+        </div>
+    )
+}
